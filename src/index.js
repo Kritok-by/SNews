c11d5944a8ae6ec7369c54b1b361248886710159
Artyom <Psixart1@gmail.com>
2020-10-21 21:18:19 +0300

    first build version

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { Reducers } from './redux/Reducers';
 import thunk from 'redux-thunk';
 
-export const store = createStore(
-  Reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+export const store = createStore(Reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
